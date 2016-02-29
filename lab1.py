823251e156336d6e272cfc9c0444c5285457bbaf
Zhenia Kolesnyk <z.kolesnyk-2018@kpi.ua>
2016-02-29 13:36:17 +0200

    Upd
    Удалил функцию createFolder, добавил константу, содердащую путь к файлам

--- lab1.py
+++ lab1.py
@@ -9,20 +9,11 @@ import logging
 import os
 
 URL = 'http://www.star.nesdis.noaa.gov/smcd/emb/vci/gvix/G04/ts_L1/ByProvince/Mean/L1_Mean_UKR.R%02d.txt'
+PATH = os.path.dirname(os.path.abspath(__file__))
 
 logging.basicConfig(level=logging.INFO)
 
-def createFolder(path):
-    os.chdir(path)
-    folder = 'data'
-    os.mkdir(folder)
-    return pjoin(path, folder)
-
-path = os.path.dirname(os.path.abspath(__file__))
-folder = createFolder(path)
-
 def createFrame():
-    os.chdir(pjoin(path, folder))
     list = glob('*.csv')
     list.sort()
     for filename in list:
@@ -40,7 +31,7 @@ for i in xrange(1, 3):
     logging.info('Downloading: %s'%url)
     vhi_url = urlopen(url)
     filename = 'vhi_id_%02d_%s.csv'%(i, datetime.now().strftime("%d.%m.%Y_%I:%M"))
-    with open(pjoin(folder, filename), 'ws') as out:
+    with open(pjoin(PATH, filename), 'ws') as out:
         out.write(vhi_url.read())
     logging.info('File %s was created'%filename)
 
