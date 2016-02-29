b03a3911a1834521741c93c636c5a7855ed9d042
Zhenia Kolesnyk <z.kolesnyk-2018@kpi.ua>
2016-02-29 12:35:02 +0200

    Hot fix

--- lab1.py
+++ lab1.py
@@ -3,10 +3,11 @@ from datetime import datetime, date, time
 from urllib2 import urlopen
 from pandas import read_csv
 from pandas import DataFrame
+from os.path import join as pjoin
 import logging
 import os
 
-url = 'http://www.star.nesdis.noaa.gov/smcd/emb/vci/gvix/G04/ts_L1/ByProvince/Mean/L1_Mean_UKR.R%02d.txt'%i
+URL = 'http://www.star.nesdis.noaa.gov/smcd/emb/vci/gvix/G04/ts_L1/ByProvince/Mean/L1_Mean_UKR.R%02d.txt'
 
 logging.basicConfig(level=logging.INFO)
 
@@ -29,14 +30,14 @@ def createFrame():
     print c[:1]
 
 for i in xrange(1, 3):
+    url = URL%i
     if i in [12, 20]:
         continue
     logging.info('Downloading: %s'%url)
     vhi_url = urlopen(url)
     filename = 'vhi_id_%02d_%s.csv'%(i, datetime.now().strftime("%d.%m.%Y_%I:%M"))
-    out = open(folder+filename, 'ws')
-    out.write(vhi_url.read())
-    out.close()
+    with open(pjoin(folder, filename), 'ws') as out:
+        out.write(vhi_url.read())
     logging.info('File %s was created'%filename)
 
 print '==='
